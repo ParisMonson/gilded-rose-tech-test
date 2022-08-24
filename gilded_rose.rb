f1ffe6c2258b42d0ae633b8dad4f4fed90f57053
Paris Monson <parismonson@yahoo.com>
2022-08-24 12:10:15 +0100

    Added 15 tests + two private methods for updating brie and Backstage passes

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -6,6 +6,16 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
+      case item.name
+      when "Aged Brie"
+        update_aged_brie(item)
+        next
+      when "Backstage passes to a TAFKAL80ETC concert"
+        update_backstage(item)
+        next
+      when "Sulfuras, Hand of Ragnaros"
+        next
+      end
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
@@ -51,6 +61,39 @@ class GildedRose
       end
     end
   end
+  private
+
+  def update_aged_brie(item)
+    if item.quality < 50 && item.sell_in >= 0
+      item.quality = item.quality + 1
+    elsif item.quality < 50 && item.sell_in < 0
+      unless item.quality == 49
+        item.quality = item.quality + 2
+      else 
+        item.quality = item.quality + 1
+      end
+    end
+    item.sell_in = item.sell_in - 1
+  end
+
+  def update_backstage(item)
+    if item.quality < 50
+      item.quality = item.quality + 1
+    end
+    if item.sell_in < 11 && item.quality < 50
+        item.quality = item.quality + 1
+    end
+    if item.sell_in < 6 && item.quality < 50
+        item.quality = item.quality + 1
+    end
+    unless item.sell_in == 0
+    item.sell_in = item.sell_in - 1
+    else
+      item.quality = 0
+      item.sell_in = item.sell_in - 1
+    end
+  end
+
 end
 
 class Item
