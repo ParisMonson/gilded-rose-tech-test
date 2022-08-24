a12d2d9d4b0c83e3927fe1089c1c5a20902dcd93
Paris Monson <parismonson@yahoo.com>
2022-08-24 12:29:41 +0100

    Added an update_normal method and implemented a case statement which links to different private methods depending on the name of the item

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -15,53 +15,23 @@ class GildedRose
         next
       when "Sulfuras, Hand of Ragnaros"
         next
-      end
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+        update_normal(item)
+        next
       end
     end
   end
   private
+
+  def update_normal(item)
+    if item.quality > 0
+      item.quality = item.quality - 1
+    end
+    if item.sell_in < 0
+      item.quality = item.quality - 1
+    end
+    item.sell_in = item.sell_in - 1
+  end
 
   def update_aged_brie(item)
     if item.quality < 50 && item.sell_in >= 0
@@ -87,7 +57,7 @@ class GildedRose
         item.quality = item.quality + 1
     end
     unless item.sell_in == 0
-    item.sell_in = item.sell_in - 1
+      item.sell_in = item.sell_in - 1
     else
       item.quality = 0
       item.sell_in = item.sell_in - 1
