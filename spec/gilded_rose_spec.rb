e628218cb4afc0bae95f8b5a326895129894d3ee
Paris Monson <parismonson@yahoo.com>
2022-08-25 14:20:42 +0100

    Added some comments to make seperate tests into categories

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,6 +1,7 @@
 require 'gilded_rose'
 
 describe GildedRose do
+  # Standard Item
   it 'does not change the name' do
     items = [Item.new('foo', 0, 0)]
     GildedRose.new(items).update_quality
@@ -26,12 +27,16 @@ describe GildedRose do
     GildedRose.new(items).update_quality
     expect(items[0].quality).to eq(0)
   end
+
+  # Sulfuras
   it 'quality/sell_in never decreases' do
     items = [Item.new('Sulfuras, Hand of Ragnaros', 20, 40)]
     GildedRose.new(items).update_quality
     expect(items[0].quality).to eq(40)
     expect(items[0].sell_in).to eq(20)
   end
+
+  # Aged Brie
   it 'increases in quality' do
     items = [Item.new('Aged Brie', 20, 40)]
     GildedRose.new(items).update_quality
@@ -52,6 +57,8 @@ describe GildedRose do
     GildedRose.new(items).update_quality
     expect(items[0].quality).to eq(50)
   end
+
+  # Backstage Passes
   it 'quality increases by 2 when sell_in == 10' do
     items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 40)]
     GildedRose.new(items).update_quality
